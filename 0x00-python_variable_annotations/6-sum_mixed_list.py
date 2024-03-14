1f5f481446dcec8f18d284b39097ab877b77030f
was <was@localhost.localdomain>
2024-03-14 21:46:53 +0300

    fixing

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Union
 """
 - takes a mixed list, adds the elements and return float
 """
 
 
-def sum_mixed_list(mxd_lst: List[int union float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ returns a sumation of all items in the array"""
     sum: float = 0
     for item in mxd_lst:
