4224de1f6a05b39af5043c2d1a0065bb0a9513bc
was <was@localhost.localdomain>
2024-03-14 20:27:45 +0300

    adding

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,7 +4,9 @@
 -takes two ints (a and b)
 -returns a float
 """
-def add(a: int, b:int) ->float:
+
+
+def add(a: int, b: int) -> float:
     """ add two integers and return a float value"""
     sum = a + b
     return float(sum)
