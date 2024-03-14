f87d0a7b72e0abf21601df7f5d4cebc6db473963
was <was@localhost.localdomain>
2024-03-14 20:25:30 +0300

    creating

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+-annotated funtion add
+-takes two ints (a and b)
+-returns a float
+"""
+def add(a: int, b:int) ->float:
+    """ add two integers and return a float value"""
+    sum = a + b
+    return float(sum)
