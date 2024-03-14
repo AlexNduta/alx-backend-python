75e628d9c08ed01b49a410ecdeec00f5b27a1225
was <was@localhost.localdomain>
2024-03-14 21:40:21 +0300

    working

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+- takes a mixed list, adds the elements and return float
+"""
+
+
+def sum_mixed_list(mxd_lst: List) -> float:
+    """ returns a sumation of all items in the array"""
+    sum: float = 0
+    for item in mxd_lst:
+        sum += item
+
+    return sum
