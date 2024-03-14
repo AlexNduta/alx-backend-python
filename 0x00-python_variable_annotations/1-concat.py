34beab54c3f514bab23891d0bf64b79802b6e7b2
was <was@localhost.localdomain>
2024-03-14 20:49:20 +0300

    concatinating

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,5 @@ takes two strings and returns the concatenated value
 
 
 def concat(string1: str, string2: str) -> str:
+    """ returns a concatenated string of the input strings"""
     return string1 + string2
