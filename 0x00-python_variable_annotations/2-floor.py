e2cf8d0a4dcb1e6ddb8a7ccb5fccfb9a3a394ba0
was <was@localhost.localdomain>
2024-03-14 20:55:17 +0300

    working

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import math
+"""
+takes a floaat and rounds it to the nearest whole number
+"""
+
+
+def floor(n: float) -> int:
+    """ retuns a whole number of the float passed"""
+    return math.floor(n)
