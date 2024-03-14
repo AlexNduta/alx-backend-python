ae6db97bc2a22d73c02a98c9d34da5cc6e171f85
was <was@localhost.localdomain>
2024-03-14 21:04:11 +0300

    taking

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+-Takes float as input and returns the  string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """ returns string represenation of the float passed"""
+    return str(n)
