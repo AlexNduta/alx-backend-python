ccd9583b5310141a26cfff06cf37963131c2ed7d
was <was@localhost.localdomain>
2024-03-14 21:28:44 +0300

    had

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
+from typing import List
 """
 - Takes a list of instegers as input the returns the sum
 """
 
 
-def sum_list(intput_list: list[float]) -> float:
+def sum_list(intput_list: List[float]) -> float:
     sum: float = 0
     for item in intput_list:
         sum += item
     return sum
-
-#print(sum_list([3.14, 1.11, 2.22]))
