d5b9dc97cfd7abda798c1160bc408ccb063ab8a5
was <was@localhost.localdomain>
2024-03-15 11:53:12 +0300

    fixing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,11 +2,14 @@
 from typing import List
 """
 - Takes a list of instegers as input the returns the sum
+- The sum retuned needs to be a float type
 """
 
 
 def sum_list(intput_list: List[float]) -> float:
-    """does a sumation of all list elements"""
+    """does a sumation of all list elements
+        - should return a float type
+    """
     sum: float = 0
     for item in intput_list:
         sum += item
