1368f153301224dce9f5bb61b87f3e0591bb594a
was <was@localhost.localdomain>
2024-03-15 18:54:26 +0300

    fixing

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@ from typing import Tuple, Optional, Union
 """ takes a string, and int or float and returns a tupple"""
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ takes a string and either int r float
         - returns a tupple
     """
