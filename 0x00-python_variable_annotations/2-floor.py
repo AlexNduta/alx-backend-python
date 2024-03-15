6bec988516bc50f83c09cbd86924385b3550c246
was <was@localhost.localdomain>
2024-03-15 11:49:40 +0300

    fixing

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 import math
-"""
--takes a floaat and rounds it to the nearest whole number
-- The returned value should be an integer value at most
-- you are required to use the math library.
--The math library has the floor()
-that rounds of the passed digit to the nearest whole number
-"""
+"""-takes a floaat and rounds it to the nearest whole number"""
 
 
 def floor(n: float) -> int:
