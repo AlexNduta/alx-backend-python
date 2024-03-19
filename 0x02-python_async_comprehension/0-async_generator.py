bfc16c6f87a2811f0597daf2b1294a60bd69a5dc
was <was@localhost.localdomain>
2024-03-19 22:16:24 +0300

    fixing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from typing import Generator
 """ module doc"""
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
-        rand_num = random.uniform(0, 10)
-        yield rand_num
+        yield random.uniform(0, 10)
