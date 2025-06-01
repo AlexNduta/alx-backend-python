b33b0753b02f082aed213875d4acb29b134b309e
was <was@localhost.localdomain>
2025-06-01 12:20:12 +0300

    testing the access_nested_map() version 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a":1}, ["a"], 1),
         ({"a":{"b": 2}}, ["a", "b"], 2),
         ({"a":{"b":2}}, ["a", "b"], 2),
-        ({}, ["a"])
         ])
     def test_access_nested_map(self, nested_map: Dict, path: Sequence, expected_result: Any) -> None:
         """
@@ -32,13 +31,13 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ["a"]),
         ({"a":1}, ["a", "b"])
         ])
-
     def test_access_nested_map_exception(self, nested_map: Dict, path: Sequence):
         """
         test if keyError is raised for the passed parameters 
         """
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
+        print(cm.exception)
 
 
 
