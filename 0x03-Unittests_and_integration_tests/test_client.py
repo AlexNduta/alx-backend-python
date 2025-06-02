ae466b003e49cdbc7689b038d37767968c6473d1
was <was@localhost.localdomain>
2025-06-02 20:09:16 +0300

    changed to the context manager with properties

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
@@ -24,29 +24,33 @@ class TestGithubOrgClient(unittest.TestCase):
         - confirm that get_jon is called once
         - Use mocks to make sure that no external HTTP calls are made
         """
-        # specify what data we should be expecting
-        # the call to the mock function 
-        #expected_payload = {"name": org_name}
-        #mock_get_json.return_value = expected_payload
-
-        # create an instance of our class so that
-        # we can be able to use the org() method
-        #client = GithubOrgClient(org_name)
-        #result = client.org
-        #self.assertEqual(result, expected_payload)
-        #expected_url = f"https://api.github.com/orgs/{org_name}"
-        # check if our mock method was called once 
-        #mock_get_json.assert_called_once(expected_url)
+        # create the mock object 'mock_get_json' that will replace the get_json()
         with patch('client.get_json' ) as mock_get_json:
+            # specify what to expect when our patch module is called
             mock_get_json.return_value = expected_payload
+            # call the main class and pass the orgaisation name
             client = GithubOrgClient(org_name)
             result = client.org
 
+            # confirm that the output given is the same as the expected payload
             self.assertEqual(result, expected_payload)
             expected_url = f"https://api.github.com/orgs/{org_name}"
-
+            # confirm that our mocked function is called once
             mock_get_json.assert_called_once()
 
+    def test_public_repos_url(self):
+        """
+        - tests that the org() returns the correct output
+        - we wil be using context managers
+        """
+        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock_org:
+            known_payload = {"repos_url": "https://api.github.com/my_org/repos"}
+            mock_org.return_value = known_payload
+
+            client = GithubOrgClient('my_org')
+            result = client._public_repos_url
+
+            self.assertEqual(result, known_payload['repos_url'])
 
 
 if __name__ == '__main__':
