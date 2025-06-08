bbfbe67f5d79ce122d8ae3f641a127dd222f7124
was <was@localhost.localdomain>
2025-06-08 10:59:27 +0300

    initial django setup and adding the rest framework to my installed apps

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest-framework',
+    'rest_framework',
+    'chats',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
