452baf9e14b0e2feed8b232a9d7fe00238d90a35
was <was@localhost.localdomain>
2025-06-26 21:51:51 +0300

    testing

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,11 +1,17 @@
-from django_filters import rest_framework as filter
-from chats.models import Message
+import django_filters
+from .models import Message
 
-class MessageFilter(filter.FilterSet):
+
+class MessageFilter(django_filters.FilterSet):
+    """ search messages on a certain criteria """
+
+    # create filter for a search date
+    start_date  = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='gte')
+
+
+    # create a filter for an end date
+    end_date = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
+
     class Meta:
         model = Message
-        fields = {
-            'message_body': ['exact', 'icontains'],
-            'sent_at': ['exact', 'gte', 'lte'],
-            'sender': ['exact'],
-        }
+        fields = ['sender', 'conversation']
--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,26 +1,29 @@
-from django.urls import include, path, re_path
-from rest_framework_nested import routers
-from .views import ConversationViewSet, MessageViewSet, UserViewSet
+from django.urls import path, include
+# import the nested routre
+from rest_framework_nested import routers 
+from .views import ConversationViewSet, MessageViewSet
 
+
+# create a router and register our viewset with it
+# viewsets are what we imported from `views.py` in this case Conversationviewset and MessageViewset
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+# register the routes to generate URL patterns
+# router.register(prefix, viewset, basename[optional])
+# Our main router will be the conversation
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
-# Create nested router
-conversations_router = routers.NestedDefaultRouter(
-    router, 
-    r'conversations', 
-    lookup='conversation'
-)
-conversations_router.register(
-    r'messages', 
-    MessageViewSet, 
-    basename='conversation-messages'
-)
+# we create a nested router
+# The nested router depends on the main router with the conversation prefix
+conversation_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+# Register the MessageViewset on the nested router
+# This will create a URL like /conversations/{conversation_pk}/messages/
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+"""
+URLs generated will be conversation/, conversation/<id>/ and messages/, messages/<id>/
+"""
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(conversations_router.urls)),
-    re_path(r'auth/', include('djoser.urls')),
-    re_path(r'auth/', include('djoser.urls.authtoken')),
-]
+urlpatterns =[
+        path('', include(router.urls)),
+        path('', include(conversation_router.urls))
+        
+        ]
